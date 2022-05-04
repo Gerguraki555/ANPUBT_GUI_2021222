2ce4a3658f1a2162b5f3f3905e4af4cfa021ad6a
danivagyok2000@stud.uni-obuda.hu <danivagyok2000@stud.uni-obuda.hu>
2022-05-04 16:48:00 +0200

    a

--- Fishy Raid/Fishy Raid/Fishy Raid/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ Fishy Raid/Fishy Raid/Fishy Raid/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -61,7 +61,7 @@ namespace Fishy_Raid {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Fishy Raid;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Fishy Raid;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
