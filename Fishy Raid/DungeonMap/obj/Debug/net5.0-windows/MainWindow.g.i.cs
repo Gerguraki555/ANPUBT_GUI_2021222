4d56ac5c894d3131d14707c15c18a01a75d2d7a3
danivagyok2000@stud.uni-obuda.hu <danivagyok2000@stud.uni-obuda.hu>
2022-05-15 22:12:15 +0200

    Changed team editor: buffok láthatók

--- Fishy Raid/DungeonMap/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ Fishy Raid/DungeonMap/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -117,7 +117,7 @@ namespace DungeonMap {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/DungeonMap;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/DungeonMap;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
